e6b6dd7cfc700343ac7fd0597df6395af1c0f441
estudiante <mtorresr@estud.usfq.edu.ec>
2026-02-26 07:54:26 -0500

    primera parte del codigo en python

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,4 @@
+nombre = input("Hola, cual es tu nombre?: ")
+edad = int(input("Cuantos años tienes?: "))
+ciudad = input("Donde vives?: ")
+print(f"Hola {nombre}, tienes {edad} años y vivies en {ciudad}")
