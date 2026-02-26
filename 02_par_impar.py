17b47293c7477d5a4271ba377a06865895ae8e52
estudiante <mtorresr@estud.usfq.edu.ec>
2026-02-26 08:07:42 -0500

    segundo archivo cargado

---
+++ 02_par_impar.py
@@ -0,0 +1,11 @@
+num = int(input("Numero: "))
+if num % 2 == 0:
+    print("PAR")
+else:
+    print("IMPAR")
+
+edad = int(input("Cuantos años tienes?: "))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
